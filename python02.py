f36bf220dc2b567ca683354654e2321e1c2260e0
Gabrielly <gabrielly2019alves12@gmail.com>
2024-12-05 09:53:54 -0300

    Add files via upload

---
+++ python02.py
@@ -0,0 +1,13 @@
+print ('festa da howden')
+
+nome_usuario = 'Gabrielly Alves'  # tipo -> str = string 
+idade_usuario = 17 # tipo -> int = integer 
+altura_usuario = 1.67 # tipo . float = ponto flutuante 
+maior_idade = False # tipo -> bool = boolean 
+
+print('seja bem vindo' , nome_usuario)
+print('sua idade é ,idade_usuario, 'anos )
+print('você tem , altura_usuario 'metros, )
+print('você é maior de idade?', maior_idade )
+    
+    
